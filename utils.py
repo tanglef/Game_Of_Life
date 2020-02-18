0097fd65c04045d01d436e7edb24461c3676c718
Tanguy Lefort <tanguy_l@outlook.fr>
2020-02-18 16:37:58 +0100

    Implement the subplots display for the without numpy part

--- utils.py
+++ utils.py
@@ -5,13 +5,12 @@ Created on Mon Feb 17 19:08:08 2020
 @author: tanguy
 description: calculation and display functions
 """
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import numpy as np
 
 def display_sub(lst,title,nrow,ncol):
-    fig, axes = plt.subplots(nrow, ncol, num=title)
+    fig, axes = plt.subplots(nrow,ncol, num=title)
     for i, ax in enumerate(axes.ravel()):   
         im = ax.imshow(lst[i], cmap='cool')
         ax.set_title("iteration "+str(i))
     plt.tight_layout()
-
-
